bc2502fa18009131f123f29429405e431a57ce45
白宇 <baiyukey@qq.com>
2022-05-27 17:23:02 +0800

    [升级]headLoader.js升级到v2.3.4,可以通过给脚本路径修改不同的参数的方法，强制清除生成的缓存数据

--- media/user/js/libs/headLoader.js
+++ media/user/js/libs/headLoader.js
@@ -15,7 +15,7 @@
  * @param {Boolean} [this.showLog=false] -是否显示加载统计(仅命令行模式可用)
  * @param {Number} [this.preload=0] -预加载开关(仅命令行模式可用) 1:预加载打开(不应用于当前页面)，0:预加载关闭（加载后立即应用于当前页面）。 默认0 。
  * @link : https://github.com/baiyukey/headLoader
- * @version : 2.3.3
+ * @version : 2.3.4
  * @copyright : http://www.uielf.com
  */
 (function(_global){
@@ -27,10 +27,10 @@
   const XHR=_global.XMLHttpRequest;
   const min=/^((192\.168|172\.([1][6-9]|[2]\d|3[01]))(\.([2][0-4]\d|[2][5][0-5]|[01]?\d?\d)){2}|10(\.([2][0-4]\d|[2][5][0-5]|[01]?\d?\d)){3})|(localhost)$/.test(_global.location.hostname) ? "" : ".min";//直接返回"min"时将无缓存机制
   const getVersion=function(_hours,_delayHours){
-    if(!_hours || _hours<=0) return new Date().getTime()+(_delayHours||0)*1000*60*60;
+    if(!_hours || _hours<=0) return new Date().getTime()+(_delayHours || 0)*1000*60*60;
     let start=new Date("2000/1/1").getTime();
     let plus=Math.ceil((new Date().getTime()-start)/(1000*60*60*_hours));
-    return start+plus*1000*60*60*_hours+(_delayHours||0)*1000*60*60;
+    return start+plus*1000*60*60*_hours+(_delayHours || 0)*1000*60*60;
   };
   const getType=_=>_.replace(/^.*\.(\w*)[?#]*.*$/,"$1");//url.parse(req.url).ext无法获取错误路径的扩展名
   let hex=function(){
@@ -707,6 +707,15 @@
     if(dataFont.length>0) initLoader.dataFont=dataFont;
     if(dataFile.length>0) initLoader.dataFile=dataFile;
     initLoader.showLog=showLog;//是否显示统计
+    let srcSearch=thisScript.src.replace(/.+\.js(.*)/,"$1");
+    if(srcSearch!==thisScript.src && srcSearch!==""){
+      let srcSearchBackup=await initLoader.db.getValue("srcSearch");
+      if((!srcSearchBackup) || (srcSearchBackup && srcSearchBackup!==srcSearch)){
+        await initLoader.db.delete();
+        await initLoader.db.setValue("srcSearch",srcSearch);
+        localStorage.clear();
+      }
+    }
     await initLoader.run();
   })();
 })((window.location.origin==="null" || window.location.origin===window.top.location.origin) ? window.top : window);
